d9bc8c0161f1db27271399a66e5810c040742e45
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-10-27 14:52:14 -0400

    Made a class of car and added a few properties

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car = new Car();
+                car.Make = "Dodge";
+                car.Model = "Challenger";
+                car.Year = 2018;
+
+            Console.WriteLine($"The make of the car is {car.Make}, the model is {car.Model}, and the year is {car.Year}");
         }
     }
 }
